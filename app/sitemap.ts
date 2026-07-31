e264475830fa7d62ea2058615c195315bf869c7c
Aryansyah Yudha <pandanwangi250@gmail.com>
2026-08-01 00:35:25 +0700

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,13 @@
+// app/sitemap.ts
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://aryawijaya.my.id', 
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
